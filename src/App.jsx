a070d6afb5ce3de0c34570dba08bd5994514f5a5
EsakkiRajM <esakkiraj1150@gmail.com>
2024-05-30 19:30:53 +0530

    router page is created in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid } from "@mui/material";
+import Navbar from "./NavBar";
+import BookingPage from "./BookingPage";
+import { AppProvider } from "./Context";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const BasicPage = () => {
+    const [searchedHotel, setSearchedHotel] = React.useState("");
+
+    return (
+      <AppProvider value={{ searchedHotel, setSearchedHotel }}>
+        <Grid item>
+          <Navbar />
+          <BookingPage />
+        </Grid>
+      </AppProvider>
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:location" element={<BasicPage />}></Route>
+        <Route path="*" element={<BasicPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
